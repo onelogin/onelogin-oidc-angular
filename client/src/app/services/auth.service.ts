c035a4becb48a87e52ab202b6647175e9de1f9ff
Rich Chetwynd <richardchetwynd@gmail.com>
2018-11-15 08:19:43 -0800

    include auto silent renew

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -38,13 +38,15 @@ export class AuthService {
 
 export function getClientSettings(): UserManagerSettings {
   return {
-    authority: 'https://openid-connect.onelogin.com/oidc',
+    authority: 'https://openid-connect.onelogin-shadow01.com/oidc',
     client_id: 'you-openid-connect-app-client-id',
     redirect_uri: 'http://localhost:4200/auth-callback',
     post_logout_redirect_uri: 'http://localhost:4200/',
     response_type: 'id_token token',
     scope: 'openid profile',
     filterProtocolClaims: true,
-    loadUserInfo: true
+    loadUserInfo: true,
+    automaticSilentRenew: true,
+    silent_redirect_uri: 'http://localhost:4200/silent-refresh.html'
   };
 }
